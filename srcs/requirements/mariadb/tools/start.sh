aad74bb7f833459e3c5e8d048e0177e5805217ec
Ivan <83094182+IrenaMora@users.noreply.github.com>
2021-08-15 14:55:44 +0300

    Update start.sh

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,14 +1,3 @@
-#service mysql start
-#mysql -e "UPDATE mysql.user SET PASSWORD = PASSWORD('JoJi001') WHERE user = 'root';"
-#mysql -e "UPDATE mysql.user SET PLUGIN = '' WHERE USER = 'root';"
-#mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-#mysql -e "CREATE USER IF NOT EXISTS 'gmother'@'%' IDENTIFIED BY 'JoJi003';"
-#mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET utf8;"
-#mysql -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';";
-#mysql -e "FLUSH PRIVILEGES;"
-#service mysql stop
-#mysqld
-
 cat << EOF > /scripts/script.sql
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 CREATE USER IF NOT EXISTS 'gmother'@'%' IDENTIFIED BY 'JoJi003';
